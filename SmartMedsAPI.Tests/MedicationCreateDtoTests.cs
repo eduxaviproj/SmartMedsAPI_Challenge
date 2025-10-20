c863c1bc3f3b5495f204c98f1235bc88502aafef
eduxaviproj <eduxavi.project@gmail.com>
2025-10-20 02:09:02 +0100

    last details

--- SmartMedsAPI.Tests/MedicationCreateDtoTests.cs
+++ SmartMedsAPI.Tests/MedicationCreateDtoTests.cs
@@ -10,13 +10,13 @@ public class MedicationCreateDtoTests
     {
         var dto = new MedicationCreateDTO { Name = "Trinox", Quantity = 0 };
         var errors = ValidationHelper.Validate(dto);
-        errors.Should().Contain(e => e.ErrorMessage!.Contains("greater than 0"));
-    }
+        errors.Should().Contain(e => e.ErrorMessage!.Contains("greater than 0")); // ASSERT: Confirma que o erro esperado (relacionado à restrição "greater than 0")
+    }                                                                             //            está presente na lista de resultados de validação
 
     [Fact]
     public void Quantity_maior_que_zero_deve_ser_valido()
     {
-        var dto = new MedicationCreateDTO { Name = "Trinox", Quantity = 1 };
+        var dto = new MedicationCreateDTO { Name = "Trinoxide", Quantity = 1 };
         var errors = ValidationHelper.Validate(dto);
         errors.Should().BeEmpty();
     }
--- SmartMedsAPI.Tests/ValidationHelper.cs
+++ SmartMedsAPI.Tests/ValidationHelper.cs
@@ -6,10 +6,10 @@ namespace SmartMedsAPI.Tests
     {
         public static IList<ValidationResult> Validate(object obj)
         {
-            var ctx = new ValidationContext(obj);
-            var results = new List<ValidationResult>();
-            Validator.TryValidateObject(obj, ctx, results, validateAllProperties: true);
-            return results; // vazia = válido; com items = inválido
+            var ctx = new ValidationContext(obj); //objetivo a validar 
+            var results = new List<ValidationResult>(); // lista de erros
+            Validator.TryValidateObject(obj, ctx, results, validateAllProperties: true); // corre DataAnnotations e se falhar, adiciona na lista
+            return results; // devolve lista de erros (se estiver vazia, o DTO passou)
         }
     }
 }
